f7fdd9d258a842ce4d1fff92f5625488d923eeab
Akash Singh <akash.s.13201@gmail.com>
2021-04-26 20:43:05 +0530

    Update Connections

--- res/db.php
+++ res/db.php
@@ -1,9 +1,9 @@
 <?php
 function dbconnect(){
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $db = "tms";
+  $servername = "remotemysql.com";
+  $username = "0u8Xxq6Xww";
+  $password = "LWVxFHZIAP";
+  $db = "0u8Xxq6Xww";
   $conn = mysqli_connect($servername, $username, $password, $db);
   if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
